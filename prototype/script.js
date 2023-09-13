02bfb872573b5b95aa9573684e55af25b3c6cc81
Anwesh Korram <45973330+Anwesh897@users.noreply.github.com>
2023-09-13 20:52:00 +0530

    some changes

---
+++ prototype/script.js
@@ -0,0 +1,26 @@
+var carousel, item
+
+carousel = document.getElementsByClassName("carousel")[0]
+item = document.getElementsByClassName("item")
+
+
+
+//Showcasing images in carousel
+function animate(x, y) {
+
+    carousel.style.transform = "rotateY(-" + 36 * x + "deg)"
+    for (let i = 0; i < item.length; i++) {
+        item[i].style.transform = "rotateY(" + 36 * x + "deg)"
+    }
+
+    setTimeout(function () {
+        if (x < y) {
+            animate(x + 1, y)
+        } else {
+            function update() {
+                console.log(x, y)
+                animate(0, 9)
+            } update()
+        }
+    }, 3000)
+} animate(0, 9)
